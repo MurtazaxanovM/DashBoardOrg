e24459cf8cfafe0336fa1e75aaf7f939201de82d
hp <hp@DESKTOP-HSBP6MK>
2022-01-19 18:42:46 +0500

    Добавьте файлы проекта.

---
+++ DashBoardOrg/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace DashBoardOrg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.xuiSuperButton1 = new XanderUI.XUISuperButton();
+            this.xuiSuperButton2 = new XanderUI.XUISuperButton();
+            this.xuiSuperButton3 = new XanderUI.XUISuperButton();
+            this.xuiSuperButton4 = new XanderUI.XUISuperButton();
+            this.xuiSuperButton5 = new XanderUI.XUISuperButton();
+            this.xuiSuperButton6 = new XanderUI.XUISuperButton();
+            this.xuiSuperButton7 = new XanderUI.XUISuperButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1139, 634);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.xuiSuperButton7);
+            this.panel2.Controls.Add(this.xuiSuperButton5);
+            this.panel2.Controls.Add(this.xuiSuperButton6);
+            this.panel2.Controls.Add(this.xuiSuperButton4);
+            this.panel2.Controls.Add(this.xuiSuperButton3);
+            this.panel2.Controls.Add(this.xuiSuperButton2);
+            this.panel2.Controls.Add(this.xuiSuperButton1);
+            this.panel2.Location = new System.Drawing.Point(25, 24);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(210, 561);
+            this.panel2.TabIndex = 0;
+            // 
+            // xuiSuperButton1
+            // 
+            this.xuiSuperButton1.BackgroundColor = System.Drawing.Color.Transparent;
+            this.xuiSuperButton1.ButtonImage = ((System.Drawing.Image)(resources.GetObject("xuiSuperButton1.ButtonImage")));
+            this.xuiSuperButton1.ButtonSmoothing = System.Drawing.Drawing2D.SmoothingMode.HighSpeed;
+            this.xuiSuperButton1.ButtonStyle = XanderUI.XUISuperButton.Style.RoundedEdges;
+            this.xuiSuperButton1.ButtonText = "DashBoard";
+            this.xuiSuperButton1.CornerRadius = 15;
+            this.xuiSuperButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.xuiSuperButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.xuiSuperButton1.Horizontal_Alignment = System.Drawing.StringAlignment.Near;
+            this.xuiSuperButton1.HoverBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(31)))), ((int)(((byte)(55)))));
+            this.xuiSuperButton1.HoverTextColor = System.Drawing.Color.White;
+            this.xuiSuperButton1.ImagePosition = XanderUI.XUISuperButton.imgPosition.Left;
+            this.xuiSuperButton1.Location = new System.Drawing.Point(32, 121);
+            this.xuiSuperButton1.Name = "xuiSuperButton1";
+            this.xuiSuperButton1.SelectedBackColor = System.Drawing.Color.LimeGreen;
+            this.xuiSuperButton1.SelectedTextColor = System.Drawing.Color.White;
+            this.xuiSuperButton1.Size = new System.Drawing.Size(140, 42);
+            this.xuiSuperButton1.SuperSelected = false;
+            this.xuiSuperButton1.TabIndex = 0;
+            this.xuiSuperButton1.TextColor = System.Drawing.Color.White;
+            this.xuiSuperButton1.Vertical_Alignment = System.Drawing.StringAlignment.Center;
+            // 
+            // xuiSuperButton2
+            // 
+            this.xuiSuperButton2.BackgroundColor = System.Drawing.Color.Transparent;
+            this.xuiSuperButton2.ButtonImage = ((System.Drawing.Image)(resources.GetObject("xuiSuperButton2.ButtonImage")));
+            this.xuiSuperButton2.ButtonSmoothing = System.Drawing.Drawing2D.SmoothingMode.HighSpeed;
+            this.xuiSuperButton2.ButtonStyle = XanderUI.XUISuperButton.Style.RoundedEdges;
+            this.xuiSuperButton2.ButtonText = "Tables";
+            this.xuiSuperButton2.CornerRadius = 15;
+            this.xuiSuperButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.xuiSuperButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.xuiSuperButton2.Horizontal_Alignment = System.Drawing.StringAlignment.Near;
+            this.xuiSuperButton2.HoverBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(31)))), ((int)(((byte)(55)))));
+            this.xuiSuperButton2.HoverTextColor = System.Drawing.Color.White;
+            this.xuiSuperButton2.ImagePosition = XanderUI.XUISuperButton.imgPosition.Left;
+            this.xuiSuperButton2.Location = new System.Drawing.Point(32, 204);
+            this.xuiSuperButton2.Name = "xuiSuperButton2";
+            this.xuiSuperButton2.SelectedBackColor = System.Drawing.Color.LimeGreen;
+            this.xuiSuperButton2.SelectedTextColor = System.Drawing.Color.White;
+            this.xuiSuperButton2.Size = new System.Drawing.Size(140, 42);
+            this.xuiSuperButton2.SuperSelected = false;
+            this.xuiSuperButton2.TabIndex = 1;
+            this.xuiSuperButton2.TextColor = System.Drawing.Color.White;
+            this.xuiSuperButton2.Vertical_Alignment = System.Drawing.StringAlignment.Center;
+            // 
+            // xuiSuperButton3
+            // 
+            this.xuiSuperButton3.BackgroundColor = System.Drawing.Color.Transparent;
+            this.xuiSuperButton3.ButtonImage = ((System.Drawing.Image)(resources.GetObject("xuiSuperButton3.ButtonImage")));
+            this.xuiSuperButton3.ButtonSmoothing = System.Drawing.Drawing2D.SmoothingMode.HighSpeed;
+            this.xuiSuperButton3.ButtonStyle = XanderUI.XUISuperButton.Style.RoundedEdges;
+            this.xuiSuperButton3.ButtonText = "DashBoard";
+            this.xuiSuperButton3.CornerRadius = 15;
+            this.xuiSuperButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.xuiSuperButton3.Horizontal_Alignment = System.Drawing.StringAlignment.Center;
+            this.xuiSuperButton3.HoverBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(31)))), ((int)(((byte)(55)))));
+            this.xuiSuperButton3.HoverTextColor = System.Drawing.Color.White;
+            this.xuiSuperButton3.ImagePosition = XanderUI.XUISuperButton.imgPosition.Left;
+            this.xuiSuperButton3.Location = new System.Drawing.Point(411, -72);
+            this.xuiSuperButton3.Name = "xuiSuperButton3";
+            this.xuiSuperButton3.SelectedBackColor = System.Drawing.Color.LimeGreen;
+            this.xuiSuperButton3.SelectedTextColor = System.Drawing.Color.White;
+            this.xuiSuperButton3.Size = new System.Drawing.Size(140, 42);
+            this.xuiSuperButton3.SuperSelected = false;
+            this.xuiSuperButton3.TabIndex = 2;
+            this.xuiSuperButton3.TextColor = System.Drawing.Color.White;
+            this.xuiSuperButton3.Vertical_Alignment = System.Drawing.StringAlignment.Center;
+            // 
+            // xuiSuperButton4
+            // 
+            this.xuiSuperButton4.BackgroundColor = System.Drawing.Color.Transparent;
+            this.xuiSuperButton4.ButtonImage = ((System.Drawing.Image)(resources.GetObject("xuiSuperButton4.ButtonImage")));
+            this.xuiSuperButton4.ButtonSmoothing = System.Drawing.Drawing2D.SmoothingMode.HighSpeed;
+            this.xuiSuperButton4.ButtonStyle = XanderUI.XUISuperButton.Style.RoundedEdges;
+            this.xuiSuperButton4.ButtonText = "DashBoard";
+            this.xuiSuperButton4.CornerRadius = 15;
+            this.xuiSuperButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.xuiSuperButton4.Horizontal_Alignment = System.Drawing.StringAlignment.Center;
+            this.xuiSuperButton4.HoverBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(31)))), ((int)(((byte)(55)))));
+            this.xuiSuperButton4.HoverTextColor = System.Drawing.Color.White;
+            this.xuiSuperButton4.ImagePosition = XanderUI.XUISuperButton.imgPosition.Left;
+            this.xuiSuperButton4.Location = new System.Drawing.Point(526, -44);
+            this.xuiSuperButton4.Name = "xuiSuperButton4";
+            this.xuiSuperButton4.SelectedBackColor = System.Drawing.Color.LimeGreen;
+            this.xuiSuperButton4.SelectedTextColor = System.Drawing.Color.White;
+            this.xuiSuperButton4.Size = new System.Drawing.Size(140, 42);
+            this.xuiSuperButton4.SuperSelected = false;
+            this.xuiSuperButton4.TabIndex = 3;
+            this.xuiSuperButton4.TextColor = System.Drawing.Color.White;
+            this.xuiSuperButton4.Vertical_Alignment = System.Drawing.StringAlignment.Center;
+            // 
+            // xuiSuperButton5
+            // 
+            this.xuiSuperButton5.BackgroundColor = System.Drawing.Color.Transparent;
+            this.xuiSuperButton5.ButtonImage = ((System.Drawing.Image)(resources.GetObject("xuiSuperButton5.ButtonImage")));
+            this.xuiSuperButton5.ButtonSmoothing = System.Drawing.Drawing2D.SmoothingMode.HighSpeed;
+            this.xuiSuperButton5.ButtonStyle = XanderUI.XUISuperButton.Style.RoundedEdges;
+            this.xuiSuperButton5.ButtonText = "Settings";
+            this.xuiSuperButton5.CornerRadius = 15;
+            this.xuiSuperButton5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.xuiSuperButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.xuiSuperButton5.Horizontal_Alignment = System.Drawing.StringAlignment.Near;
+            this.xuiSuperButton5.HoverBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(31)))), ((int)(((byte)(55)))));
+            this.xuiSuperButton5.HoverTextColor = System.Drawing.Color.White;
+            this.xuiSuperButton5.ImagePosition = XanderUI.XUISuperButton.imgPosition.Left;
+            this.xuiSuperButton5.Location = new System.Drawing.Point(35, 370);
+            this.xuiSuperButton5.Name = "xuiSuperButton5";
+            this.xuiSuperButton5.SelectedBackColor = System.Drawing.Color.LimeGreen;
+            this.xuiSuperButton5.SelectedTextColor = System.Drawing.Color.White;
+            this.xuiSuperButton5.Size = new System.Drawing.Size(140, 42);
+            this.xuiSuperButton5.SuperSelected = false;
+            this.xuiSuperButton5.TabIndex = 5;
+            this.xuiSuperButton5.TextColor = System.Drawing.Color.White;
+            this.xuiSuperButton5.Vertical_Alignment = System.Drawing.StringAlignment.Center;
+            // 
+            // xuiSuperButton6
+            // 
+            this.xuiSuperButton6.BackgroundColor = System.Drawing.Color.Transparent;
+            this.xuiSuperButton6.ButtonImage = ((System.Drawing.Image)(resources.GetObject("xuiSuperButton6.ButtonImage")));
+            this.xuiSuperButton6.ButtonSmoothing = System.Drawing.Drawing2D.SmoothingMode.HighSpeed;
+            this.xuiSuperButton6.ButtonStyle = XanderUI.XUISuperButton.Style.RoundedEdges;
+            this.xuiSuperButton6.ButtonText = "Profile";
+            this.xuiSuperButton6.CornerRadius = 15;
+            this.xuiSuperButton6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.xuiSuperButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.xuiSuperButton6.Horizontal_Alignment = System.Drawing.StringAlignment.Near;
+            this.xuiSuperButton6.HoverBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(31)))), ((int)(((byte)(55)))));
+            this.xuiSuperButton6.HoverTextColor = System.Drawing.Color.White;
+            this.xuiSuperButton6.ImagePosition = XanderUI.XUISuperButton.imgPosition.Left;
+            this.xuiSuperButton6.Location = new System.Drawing.Point(35, 287);
+            this.xuiSuperButton6.Name = "xuiSuperButton6";
+            this.xuiSuperButton6.SelectedBackColor = System.Drawing.Color.LimeGreen;
+            this.xuiSuperButton6.SelectedTextColor = System.Drawing.Color.White;
+            this.xuiSuperButton6.Size = new System.Drawing.Size(140, 42);
+            this.xuiSuperButton6.SuperSelected = false;
+            this.xuiSuperButton6.TabIndex = 4;
+            this.xuiSuperButton6.TextColor = System.Drawing.Color.White;
+            this.xuiSuperButton6.Vertical_Alignment = System.Drawing.StringAlignment.Center;
+            // 
+            // xuiSuperButton7
+            // 
+            this.xuiSuperButton7.BackgroundColor = System.Drawing.Color.Transparent;
+            this.xuiSuperButton7.ButtonImage = ((System.Drawing.Image)(resources.GetObject("xuiSuperButton7.ButtonImage")));
+            this.xuiSuperButton7.ButtonSmoothing = System.Drawing.Drawing2D.SmoothingMode.HighSpeed;
+            this.xuiSuperButton7.ButtonStyle = XanderUI.XUISuperButton.Style.RoundedEdges;
+            this.xuiSuperButton7.ButtonText = "Log Out";
+            this.xuiSuperButton7.CornerRadius = 15;
+            this.xuiSuperButton7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.xuiSuperButton7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.xuiSuperButton7.Horizontal_Alignment = System.Drawing.StringAlignment.Near;
+            this.xuiSuperButton7.HoverBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(31)))), ((int)(((byte)(55)))));
+            this.xuiSuperButton7.HoverTextColor = System.Drawing.Color.White;
+            this.xuiSuperButton7.ImagePosition = XanderUI.XUISuperButton.imgPosition.Left;
+            this.xuiSuperButton7.Location = new System.Drawing.Point(35, 453);
+            this.xuiSuperButton7.Name = "xuiSuperButton7";
+            this.xuiSuperButton7.SelectedBackColor = System.Drawing.Color.LimeGreen;
+            this.xuiSuperButton7.SelectedTextColor = System.Drawing.Color.White;
+            this.xuiSuperButton7.Size = new System.Drawing.Size(140, 42);
+            this.xuiSuperButton7.SuperSelected = false;
+            this.xuiSuperButton7.TabIndex = 6;
+            this.xuiSuperButton7.TextColor = System.Drawing.Color.White;
+            this.xuiSuperButton7.Vertical_Alignment = System.Drawing.StringAlignment.Center;
+            this.xuiSuperButton7.Click += new System.EventHandler(this.xuiSuperButton7_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(32, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(143, 59);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1139, 634);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private XanderUI.XUISuperButton xuiSuperButton7;
+        private XanderUI.XUISuperButton xuiSuperButton5;
+        private XanderUI.XUISuperButton xuiSuperButton6;
+        private XanderUI.XUISuperButton xuiSuperButton4;
+        private XanderUI.XUISuperButton xuiSuperButton3;
+        private XanderUI.XUISuperButton xuiSuperButton2;
+        private XanderUI.XUISuperButton xuiSuperButton1;
+    }
+}
+
